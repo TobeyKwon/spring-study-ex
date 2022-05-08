a84b917c3354f36fa72a3db8ce9c4f80c265104f
TobeyKwon <tobeykwon@gmail.com>
2022-05-08 20:06:52 +0900

    exception firstcommit

---
+++ sxception/src/main/java/hello/sxception/SxceptionApplication.java
@@ -0,0 +1,13 @@
+package hello.sxception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SxceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SxceptionApplication.class, args);
+	}
+
+}
---
+++ sxception/src/test/java/hello/sxception/SxceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.sxception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SxceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
